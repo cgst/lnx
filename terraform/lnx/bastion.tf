5949fa6d3452dcb2d618f393a4a7e425c8fcb05b
Cristian Strat <cristian.strat@coinbase.com>
2019-02-09 16:47:55 -0800

    Add bastion to SSH to ECS instances

---
+++ terraform/lnx/bastion.tf
@@ -0,0 +1,48 @@
+module "bastion" {
+  source            = "github.com/segmentio/stack/bastion"
+  region            = "${var.region}"
+  security_groups   = "${aws_security_group.bastion.id}"
+  vpc_id            = "${module.ecs.vpc_id}"
+  key_name          = "${aws_key_pair.ecs.key_name}"
+  subnet_id         = "${module.ecs.vpc_public_subnet_ids[0]}"
+  environment       = "${var.environment}"
+}
+
+# Bastion security group allows SSH access from anywhere.
+resource "aws_security_group" "bastion" {
+  name        = "${format("%s-bastion", var.environment)}"
+  vpc_id      = "${module.ecs.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags {
+    Name        = "${format("%s-bastion", var.environment)}"
+    Environment = "${var.environment}"
+  }
+}
+
+# Allow SSH access to ECS instances from bastion.
+resource "aws_security_group_rule" "inbound_ssh_access_from_bastion" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  source_security_group_id = "${aws_security_group.bastion.id}"
+  security_group_id = "${module.ecs.ecs_instance_security_group_id}"
+}
