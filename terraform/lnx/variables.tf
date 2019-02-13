c13e9e14d3be3bc89ba7313797e3ae598652c75e
Cristian Strat <cristian.strat@coinbase.com>
2019-02-12 19:23:30 -0800

    New regions: ap-southeast-1, eu-central-1, sa-east-1

--- terraform/lnx/variables.tf
+++ terraform/lnx/variables.tf
@@ -9,25 +9,37 @@ variable "instance_type" {
 
 variable "ecs_ami" {
   type = "map"
+
   default = {
-    "us-east-1" = "ami-0bf2fb355727b7faf"
-    "us-west-1" = "ami-082091011e69ea8a8"
+    "us-east-1"      = "ami-0bf2fb355727b7faf"
+    "us-west-1"      = "ami-0c9bd36a7394439a6"
+    "ap-southeast-1" = "ami-00ae2723e3c86c93e"
+    "eu-central-1"   = "ami-0ce9ac8aed24e9ee5"
+    "sa-east-1"      = "ami-0ec252e7e0588e54d"
   }
 }
 
 variable "bitcoind_snapshot" {
   type = "map"
+
   default = {
-    "us-east-1" = "snap-0ca6c5b445ac33986"
-    "us-west-1" = "snap-05f72c2715d43df0e"
+    "us-east-1"      = "snap-0ca6c5b445ac33986"
+    "us-west-1"      = "snap-05f72c2715d43df0e"
+    "ap-southeast-1" = "snap-04b123964bba32019"
+    "eu-central-1"   = "snap-0c6dec8a1ca1abee1"
+    "sa-east-1"      = "snap-0d7b82588c404798f"
   }
 }
 
 variable "lnd_snapshot" {
   type = "map"
+
   default = {
-    "us-east-1" = "snap-0a3dad16496e68a06"  # Empty ext4
-    "us-west-1" = "snap-0bc81c2b0cdde7248"  # Empty ext4
+    "us-east-1"      = "snap-0a3dad16496e68a06" # Empty ext4
+    "us-west-1"      = "snap-0bc81c2b0cdde7248" # Empty ext4
+    "ap-southeast-1" = "snap-03e10c98dd326a6d1" # Empty ext4
+    "eu-central-1"   = "snap-0d444401da1e7e2c0" # Empty ext4
+    "sa-east-1"      = "snap-097c5a2873070fbc0" # Empty ext4
   }
 }
 
@@ -36,12 +48,12 @@ variable "vpc_cidr" {
 }
 
 variable "private_subnet_cidrs" {
-  type = "list"
+  type    = "list"
   default = ["10.0.50.0/24", "10.0.51.0/24"]
 }
 
 variable "public_subnet_cidrs" {
-  type = "list"
+  type    = "list"
   default = ["10.0.0.0/24", "10.0.1.0/24"]
 }
 
