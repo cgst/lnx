9ba17fe882bc341c8871b9401de2a89c29a2347c
Cristian Strat <cristian.strat@coinbase.com>
2019-04-24 20:06:32 -0700

    Remove unused lnx instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,75 +30,11 @@ module "us-west" {
     aws = "aws.us-west-1"
   }
 
-  ln_banner               = "2.ln.glass"
-  public_hostname         = "2.ln.glass"
+  ln_banner               = "1.ln.glass"
+  public_hostname         = "1.ln.glass"
   availability_zones      = ["us-west-1a", "us-west-1c"]
   environment             = "${var.environment}"
   ssh_public_key          = "${var.ssh_public_key}"
   iam_instance_profile_id = "${aws_iam_instance_profile.ecs.id}"
   instance_type           = "m5.large"                           # m5a not available
 }
-
-//module "us-east" {
-//  source                  = "lnx"
-//  region                  = "us-east-1"
-//
-//  ln_banner               = "1.ln.glass"
-//  public_hostname         = "1.ln.glass"
-//  availability_zones      = ["us-east-1a", "us-east-1b"]
-//  environment             = "${var.environment}"
-//  ssh_public_key          = "${var.ssh_public_key}"
-//  iam_instance_profile_id = "${aws_iam_instance_profile.ecs.id}"
-//  instance_type           = "m5a.large"
-//}
-//
-//module "ap-southeast" {
-//  source                  = "lnx"
-//  region                  = "ap-southeast-1"
-//
-//  providers = {
-//    aws = "aws.ap-southeast-1"
-//  }
-//
-//  ln_banner               = "3.ln.glass"
-//  public_hostname         = "3.ln.glass"
-//  availability_zones      = ["ap-southeast-1b", "ap-southeast-1c"]
-//  environment             = "${var.environment}"
-//  ssh_public_key          = "${var.ssh_public_key}"
-//  iam_instance_profile_id = "${aws_iam_instance_profile.ecs.id}"
-//  instance_type           = "m5a.large"
-//}
-
-//module "eu-central" {
-//  source                  = "lnx"
-//  region                  = "eu-central-1"
-//
-//  providers = {
-//    aws = "aws.eu-central-1"
-//  }
-//
-//  ln_banner               = "4.ln.glass"
-//  public_hostname         = "4.ln.glass"
-//  availability_zones      = ["eu-central-1b", "eu-central-1c"]
-//  environment             = "${var.environment}"
-//  ssh_public_key          = "${var.ssh_public_key}"
-//  iam_instance_profile_id = "${aws_iam_instance_profile.ecs.id}"
-//  instance_type           = "m5.large"                           # m5a not available
-//}
-//
-//module "sa-east" {
-//  source                  = "lnx"
-//  region                  = "sa-east-1"
-//
-//  providers = {
-//    aws = "aws.sa-east-1"
-//  }
-//
-//  ln_banner               = "5.ln.glass"
-//  public_hostname         = "5.ln.glass"
-//  availability_zones      = ["sa-east-1b", "sa-east-1c"]
-//  environment             = "${var.environment}"
-//  ssh_public_key          = "${var.ssh_public_key}"
-//  iam_instance_profile_id = "${aws_iam_instance_profile.ecs.id}"
-//  instance_type           = "m5.large"                           # m5a not available
-//}
